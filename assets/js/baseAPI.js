8531e9fb6167ec96b1eafa4ed2503b5662000d54
cyz <cyz@itcast.cn>
2021-01-18 17:03:20 +0800

    完成了首页的功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,19 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    // 为有权限的接口统一设置headers请求头
+    if(options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 控制用户的访问权限
+    // 无论成功失败都会访问complete函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 强制清空本地存储
+            localStorage.removeItem('token');
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(function () {
+    //  1.获取信息渲染页面
+    getUserinfo()
+    // 2.实现退出功能
+    $('#btnLogout').on('click',function() {
+        // 弹出询问框
+        layer.confirm('确认退出登录吗?', {icon: 3, title:'提示'}, function(index){
+            // 删除本地存储中的token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+            // 关闭询问框
+            layer.close(index);
+          });
+    })
+})
+// 封装获取函数
+function getUserinfo() {
+    // 发送ajax请求
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取数据失败')
+            }
+            // 如果获取数据成功 就渲染页面
+            renderAvatar(res.data)
+        },
+    });
+}
+// 封装渲染页面的函数
+function renderAvatar(user) {
+    // 获取用户名 用来渲染欢迎词
+    var name = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 按需渲染头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 取出name里面的第一个字符转换为大写将他作为文字头像显示
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+        $('.layui-nav-img').hide();
+    }
+}
